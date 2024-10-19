ae9bd9af21ceaeaa43b3c540ca88413e07898547
ArtChaos189 <tvolkiska967@gmail.com>
2024-10-19 19:19:25 +0300

    feat: add copy button

--- src/widgets/two-factor-auth-list/ui/two-factor-auth-item/ui/two-factor-auth-item.tsx
+++ src/widgets/two-factor-auth-list/ui/two-factor-auth-item/ui/two-factor-auth-item.tsx
@@ -1,24 +1,43 @@
+import { useState } from "react";
+
 import { Progressbar } from "@/shared/progressbar";
+
+import CopyIcon from "@/shared/assets/copy.png";
 
 import "./two-factor-auth-item.sass";
 
 type TwoFactorAuthItemProps = {
-  icon: string;
+  icon: string | null;
   name: string;
   code: string;
 };
 
 export const TwoFactorAuthItem = ({ icon, name, code }: TwoFactorAuthItemProps) => {
-  const handleCopy = () => navigator.clipboard.writeText(code);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(code);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 5000);
+  };
 
   return (
     <button className="wrapper" onClick={handleCopy}>
-      <img className="icon" src={icon} alt="icon" />
+      <img className="icon" src={icon as string} alt="icon" />
       <div className="inner-wrapper">
         <h5 className="name">{name}</h5>
         <p className="code">{code}</p>
       </div>
-      <Progressbar />
+      <div className="actions">
+        {isCopied && (
+          <button className="copy-btn" onClick={handleCopy}>
+            <img src={CopyIcon} alt="icon" />
+          </button>
+        )}
+        <Progressbar />
+      </div>
     </button>
   );
 };
