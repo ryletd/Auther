edabb6d184b89a62603f941e1209001f725ab125
ArtChaos189 <tvolkiska967@gmail.com>
2024-10-24 00:46:09 +0300

    style: Upload

--- src/shared/assets/svg/close.tsx
+++ src/shared/assets/svg/close.tsx
@@ -1,7 +1,13 @@
 import { FC, SVGProps } from "react";
 
 export const CloseIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
-  <svg {...props} height="1em" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M17 1L1 17M17 17L1 1L17 17Z" stroke="#ff3838" strokeWidth="2" strokeLinecap="round" />
+  <svg {...props} height="1em" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M17 1L1 17M17 17L1 1L17 17Z"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      fill="currentColor"
+    />
   </svg>
 );
