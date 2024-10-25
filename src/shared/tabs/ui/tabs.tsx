a3ed549d829cb65a7f68a299c2762634ff2d7a00
ArtChaos189 <tvolkiska967@gmail.com>
2024-10-25 19:44:39 +0300

    feat: comments

--- src/shared/tabs/ui/tabs.tsx
+++ src/shared/tabs/ui/tabs.tsx
@@ -3,26 +3,24 @@ import classNames from "classnames";
 import "./tabs.sass";
 
 type TabsType = {
+  value: number;
   buttons: string[];
   onChange: (index: number) => void;
-  value: number;
 };
 
-export const Tabs = ({ buttons, onChange, value }: TabsType) => {
-  return (
-    <div className="tabs">
-      <div className="wrapper-buttons">
-        {buttons.map((button, index) => (
-          <button
-            key={button + index}
-            type="button"
-            onClick={() => onChange(index)}
-            className={classNames("tabs-button", { active: value === index })}
-          >
-            {button}
-          </button>
-        ))}
-      </div>
+export const Tabs = ({ buttons, onChange, value }: TabsType) => (
+  <div className="tabs">
+    <div className="wrapper-buttons">
+      {buttons.map((button, index) => (
+        <button
+          key={button + index}
+          type="button"
+          onClick={() => onChange(index)}
+          className={classNames("tabs-button", { active: value === index })}
+        >
+          {button}
+        </button>
+      ))}
     </div>
-  );
-};
+  </div>
+);
